013e642ef96666bfd8b6540d030244893bff0960
nguyen khanh linh <125460670+borine06@users.noreply.github.com>
2023-03-07 18:11:24 +0700

    Add files via upload

---
+++ baitap4.java
@@ -0,0 +1,14 @@
+package bt;
+import java.util.Scanner;
+
+public class baitap4 {
+	public static void main(String[] args) {
+        int n;
+        Scanner input = new Scanner(System.in);
+        do {
+            System.out.println("Nhap n:");
+            n = input.nextInt();
+        } while(n < 1 || n > 12);
+        System.out.println("Thang " + n);
+    }
+}
